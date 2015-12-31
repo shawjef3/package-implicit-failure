0843b4f8962006cf7bc0dfd9e17b6228d24d2aa1
Jeff Shaw <shawjef3@gmail.com>
2015-12-30 21:09:49 -0500

    add a test that imports a1

---
+++ src/test/scala/me/jeffshaw/ImportedA1Spec.scala
@@ -0,0 +1,12 @@
+package me.jeffshaw
+
+import org.scalatest.FunSuite
+import me.jeffshaw.a1._
+
+class ImportedA1Spec extends FunSuite {
+
+  test("implicit A is found in package object with import") {
+    assertCompiles("implicitly[A]")
+  }
+
+}
